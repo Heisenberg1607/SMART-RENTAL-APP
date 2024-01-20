81dbaf3cdd5b930a5c3e9ec7f3c5bd2d909b2444
Ashking07 <ashwinkapile2002@gmail.com>
2024-01-20 18:42:46 +0530

    CSS Added to Nav-Bar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,26 +1,93 @@
-import React from 'react'
-import "../Navbar.css";
-import Link from "next/link";
-const Navbar = () => {
-  return (
-    <>
-      <div className="navbar">
-        <div>
-          <div className="logo"> LOGO </div>
-        </div>
-        <div className="list">
-        <ul className="links">
-            <li><Link href="/">About us</Link></li>
-            <li><Link href="SignUp">Sign Up</Link></li>
-            <li><Link href="">Login</Link></li>
-        </ul>
-        </div>
-        <div>
-          <input type="text" />
-        </div>
-      </div>
-    </>
-  );
-}
+// import React from "react";
+// import "../Navbar.css";
+// import Link from "next/link";
+// import { Button, Typography } from "@mui/material";
+// import { Container } from "postcss";
 
-export default Navbar
+// const Navbar = () => {
+//   return (
+//     <>
+//       <div className="navbar">
+//         <div>
+//           <div className="logo"> LOGO </div>
+//         </div>
+//         <div className="list">
+//           <ul className="links">
+//             <li>
+//               <Link href="/">About us</Link>
+//             </li>
+//             <li>
+//               <Link href="SignUp">Sign Up</Link>
+//             </li>
+//             <li>
+//               <Link href="">Login</Link>
+//             </li>
+//           </ul>
+//         </div>
+//         <div>
+//           <input type="text" />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Navbar;
+
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Container,
+  Link,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const Navbar = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <AppBar position="static" color="primary">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Smart Rental App Logo
+          </Typography>
+          <Link href="/" color="inherit">
+            <Typography variant="subtitle1" sx={{ ml: 2 }}>
+              About Us
+            </Typography>
+          </Link>
+          <Link href="SignUp" color="inherit">
+            <Typography variant="subtitle1" sx={{ ml: 2 }}>
+              Sign Up
+            </Typography>
+          </Link>
+          <Link href="/login" color="inherit">
+            <Typography variant="subtitle1" sx={{ ml: 2, mr: 2 }}>
+              Login
+            </Typography>
+          </Link>
+          <div>
+            <input type="text" placeholder="Search" />
+          </div>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Navbar;
