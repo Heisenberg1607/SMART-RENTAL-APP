a9d8938915bb7af2565e7f80d1ae3e893b96a9df
Ashking07 <ashwinkapile2002@gmail.com>
2024-01-20 18:44:02 +0530

    CSS added to NavBar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,39 +1,3 @@
-// import React from "react";
-// import "../Navbar.css";
-// import Link from "next/link";
-// import { Button, Typography } from "@mui/material";
-// import { Container } from "postcss";
-
-// const Navbar = () => {
-//   return (
-//     <>
-//       <div className="navbar">
-//         <div>
-//           <div className="logo"> LOGO </div>
-//         </div>
-//         <div className="list">
-//           <ul className="links">
-//             <li>
-//               <Link href="/">About us</Link>
-//             </li>
-//             <li>
-//               <Link href="SignUp">Sign Up</Link>
-//             </li>
-//             <li>
-//               <Link href="">Login</Link>
-//             </li>
-//           </ul>
-//         </div>
-//         <div>
-//           <input type="text" />
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Navbar;
-
 import React from "react";
 import {
   AppBar,
