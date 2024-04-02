c505b2d7724555da2b1e7dea15098f9fd8669595
Ashking07 <ashwinkapile2002@gmail.com>
2024-04-02 12:21:16 +0530

    Homepage improved.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,10 +11,23 @@ export default function Home() {
   return (
     <>
       <div className="home-page">
-        <h1 className="hero-headline text-black absolute top-16 tracking-widest">
+        <h1 className="text-5xl text-stone-900 mt-7 absolute top-16 tracking-widest">
           Smart Rental Application.Co
         </h1>
-
+        <p className="text-center w-[850px] font-normal text-lg mt-0 tracking-wider mb-16">
+          Welcome to{" "}
+          <strong className="text-blue-900 text-md">
+            Smart Rental Application.Co
+          </strong>
+          , your premier destination for streamlined rental solutions powered by
+          <strong className="capitalize"> blockchain technology</strong>. Our
+          innovative platform revolutionizes the rental process, offering secure
+          transactions and transparent agreements for both property owners and
+          renters. With intuitive features and user-friendly navigation, we're
+          dedicated to simplifying every aspect of renting, from property search
+          to lease agreements. Join us today and experience the future of
+          renting with ease and confidence.
+        </p>
         <form
           onSubmit={() => setName("")}
           className="flex-col text-center mb-4"
@@ -26,7 +39,7 @@ export default function Home() {
             type="text"
             value={userName}
             onChange={handleUserName}
-            className="rounded-full p-2 transition-all duration-300 focus:outline-none focus:border-purple-400 focus:ring-1 focus:ring-purple-400 placeholder:text-sm"
+            className="rounded-full p-2 transition-all duration-300 focus:outline-none focus:border-purple-400 focus:ring-1 focus:ring-purple-400 placeholder:text-sm mb-0"
             placeholder="Enter Your Name..."
           ></input>
         </form>
