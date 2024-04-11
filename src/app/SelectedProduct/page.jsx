9ecdde44982fa481cfeed7b02b1cdfc668e57bcd
atha10 <atharvamsc.24@gmail.com>
2024-04-11 13:36:47 +0530

    fix

--- src/app/SelectedProduct/page.jsx
+++ src/app/SelectedProduct/page.jsx
@@ -17,9 +17,21 @@ import {
 import { useRouter } from "next/navigation";
 import Button from "../components/Button";
 import Loader from "../components/Loader";
+import { constants, ethers } from "ethers";
+import Web3Modal from "web3modal";
+import contract from "../Context/TransferEth.json";
+// import contract from "../Context/RecieveEth.json";
+const ContractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const ContractABI =  contract.abi;
+const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+//Fetching Contact from blockchain
+const fetchContract = (signerOrProvider) =>
+  new ethers.Contract(ContractAddress,ContractABI,signerOrProvider)
 
 const page = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
   const [data, setData] = useState([]);
+  const [ownerDetails, setOwnerDetails] = useState([]);
   const [imageUrl, setImageUrl] = useState(null);
   const [item, setItem] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -44,6 +56,7 @@ const page = () => {
   }, []);
 
   useEffect(() => {
+    checkWalletIsConnected();
     const fetchProductData = async () => {
       try {
         const docRef = doc(db, "products", `${p_id}`);
@@ -58,8 +71,23 @@ const page = () => {
       }
     };
 
+    const fetchOwnerAddress = async () => {
+      try {
+        const docRef = doc(db, "users", `${p_id}`);
+
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setOwnerDetails(docSnap.data());
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     if (p_id) {
       fetchProductData();
+      fetchOwnerAddress();
     }
   }, []);
 
@@ -80,6 +108,50 @@ const page = () => {
     };
     fetchImageUrl();
   }, [data.email]);
+
+  const transferProcess = async () => {
+    if( await checkWalletIsConnected()){
+      const signer = provider.getSigner()
+      const address = signer.getAddress();
+      const contract = await fetchContract(signer);
+      const ownerAddress = ownerDetails.walletAddress
+      console.log('sender address - ' + address)
+      console.log(ownerAddress)
+      let price = data.itemPrice
+      price = '' + price
+      console.log(price)
+      const options = { value: ethers.utils.parseEther(price) }
+      // const trancaction = await contract.transferEther(ownerAddress,options)
+      // const trancaction = await contract.getBalance()
+        const trancaction = await contract.transferEther(ownerAddress,options)
+      // const trancaction3 = await contract.withdrawMoney()
+      const trancaction2 = await contract.getBalance()
+      console.log(trancaction)
+      
+    }else{
+      setErrorMessage("Please Connect to Metamask!!!");
+    }
+
+  };
+
+  const checkWalletIsConnected = async () => {
+    if (window.ethereum || window.ethereum.isConnected()) {
+      const account = await window.ethereum.request({method: "eth_accounts"})
+      if (account.length){
+        return true;
+      }
+      else{
+          setErrorMessage("Please Connect to Metamask!!!");
+          provider.send("eth_requestAccounts", []).then(async () => {
+            return true;
+          });
+          return false;
+      }
+    } else {
+      setErrorMessage("Please Install Metamask!!!");
+      return false;
+    }
+  };
 
   return (
     <>
@@ -107,7 +179,7 @@ const page = () => {
                   <Button onClick={handleBack} type="goBack" className="mb-4">
                     👈 Go Back
                   </Button>
-                  <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300">
+                  <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300" onClick={()=>transferProcess()}>
                     Rent this product
                   </button>
                 </div>
