db933cc54cf75ba065fe552611da54d1f71544ef
Heisenberg1607 <76523428+Heisenberg1607@users.noreply.github.com>
2024-05-03 15:10:44 +0530

    confirm-order page added

---
+++ src/app/ConfirmOrder/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./page.css"
+
+const page = () => {
+  return (
+      <div className="container-class">
+          <div className="confirm-text">
+              Your Order is confirmed! Thank you for using our app! 
+          </div>
+    </div>
+  )
+}
+
+export default page
